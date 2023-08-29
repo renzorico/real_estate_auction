3561e1c3c0a0edff33a5e343416c231ea377dd94
Renzo Rico <renzorico10@gmail.com>
2023-08-29 15:58:32 +0200

    streamlit modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,36 @@ import pandas as pd
 # Read the DataFrame with geocoded coordinates
 df = pd.read_csv('data/propiedades_geocoded.csv')
 
-# Display the DataFrame
-st.write('## Properties Data')
-st.dataframe(df)
+# Get unique values for 'Tipo de subasta' and 'Provincia'
+auction_types = df['Tipo de subasta'].unique()
+provinces = df['Provincia'].unique()
 
-# Calculate the center based on median latitude and longitude
+# Create Streamlit app interface
+st.sidebar.header('Filtros')
+
+# Filter options in the sidebar
+selected_auction_type = st.sidebar.selectbox('Seleccionar tipo de subasta', ['TODOS'] + list(auction_types))
+selected_province = st.sidebar.selectbox('Seleccionar provincia', ['TODOS'] + list(provinces))
+apply_filters = st.sidebar.button('Confirmar', key="apply_button")
+
+# Apply the filters to the DataFrame
+if apply_filters:
+    if selected_auction_type == 'TODOS' and selected_province == 'TODOS':
+        filtered_df = df
+    elif selected_auction_type == 'TODOS':
+        filtered_df = df[df['Provincia'] == selected_province]
+    elif selected_province == 'TODOS':
+        filtered_df = df[df['Tipo de subasta'] == selected_auction_type]
+    else:
+        filtered_df = df[(df['Tipo de subasta'] == selected_auction_type) & (df['Provincia'] == selected_province)]
+else:
+    filtered_df = df
+
+# Display the DataFrame with applied filters
+st.write('## Propiedades disponibles')
+st.dataframe(filtered_df)
+
+# Calculate the center based on median latitude and longitude of filtered data
 def get_latitude(coord):
     if isinstance(coord, str):
         return float(coord.split(',')[0])
@@ -23,15 +48,18 @@ def get_longitude(coord):
     else:
         return coord
 
-median_latitude = df['Coordinates'].apply(get_latitude).median()
-median_longitude = df['Coordinates'].apply(get_longitude).median()
+if not filtered_df.empty:
+    median_latitude = filtered_df['Coordinates'].apply(get_latitude).median()
+    median_longitude = filtered_df['Coordinates'].apply(get_longitude).median()
 
-# Display the map
-st.write('## Property Map')
-m = folium.Map(location=[median_latitude, median_longitude], zoom_start=12)
-for index, row in df.iterrows():
-    coordinates = row['Coordinates']
-    if isinstance(coordinates, str):
-        latitude, longitude = map(float, coordinates.split(', '))
-        folium.Marker([latitude, longitude], popup=row['Dirección Mapa']).add_to(m)
-folium_static(m)
+    # Display the map
+    st.write('## Mapa propiedades disponibles')
+    m = folium.Map(location=[median_latitude, median_longitude], zoom_start=12, min_zoom=3, max_zoom=15)
+    for index, row in filtered_df.iterrows():
+        coordinates = row['Coordinates']
+        if isinstance(coordinates, str):
+            latitude, longitude = map(float, coordinates.split(', '))
+            folium.Marker([latitude, longitude], popup=row['Identificador']).add_to(m)
+    folium_static(m)
+else:
+    st.write("No hay propiedades disponibles para esta selección.")
